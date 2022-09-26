8417f39e7d777a8f0a85d22b99edded9328a086b
chysreax <yogadewa0000@gmail.com>
2022-09-26 09:11:34 +0700

    Update 1_unit-tests.js

--- boilerplate-mochachai/tests/1_unit-tests.js
+++ boilerplate-mochachai/tests/1_unit-tests.js
@@ -5,23 +5,5 @@ suite('Unit Tests', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.isNotNull(1, '1 is not null');
     });
-    // #2
-    test('#isDefined, #isUndefined', function () {
-      assert.isNull(null, 'null is not undefined');
-      assert.isUndefined(undefined, 'undefined IS undefined');
-      assert.isDefined('hello', 'A string is not undefined');
-    });
-    // #3
-    test('#isOk, #isNotOk', function () {
-      assert.isNull(null, 'null is falsey');
-      assert.isOk("I'm truthy", 'A string is truthy');
-      assert.isNotOk(true, 'true is truthy');
-    });
-    // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.isTrue(true, 'true is true');
-      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-    });
   });
 });
